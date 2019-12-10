a204b377076763289a16e24efb1438afe8ba629a
Genaro Coronel <genaro.coronel561@gmail.com>
2019-12-10 14:19:59 -0800

    Added initial foo route with simple hello world GET support

---
+++ server/src/foo/foo.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FooController } from './foo.controller';
+import { FooService } from './foo.service';
+
+describe('Foo Controller', () => {
+  let controller: FooController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [FooController],
+      providers: [FooService],
+    }).compile();
+
+    controller = module.get<FooController>(FooController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('foo', () => {
+    it('should return "Hello Foo!"', () => {
+      expect(controller.getHello()).toBe('Hello Foo!');
+    });
+  });
+});
