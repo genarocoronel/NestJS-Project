47598fbe6e432e1af18fa7288e4d2dac921f0807
Genaro Coronel <genaro.coronel561@gmail.com>
2019-12-10 14:36:34 -0800

    Fixed file name typo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FooModule } from './foo/foo,module';
+import { FooModule } from './foo/foo.module';
 
 @Module({
   imports: [FooModule],
